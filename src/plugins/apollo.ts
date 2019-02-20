2f6c80d5620b919c7dd5bcf5e64fb64407f8c8af
Chris Bradley <chris_bradley@sweetwater.com>
2019-02-20 08:04:41 -0500

    update apollo

--- src/plugins/apollo.ts
+++ src/plugins/apollo.ts
@@ -1,7 +1,6 @@
 import "isomorphic-fetch";
 import Vue from "vue";
 import ApolloSSR from "vue-apollo/ssr";
-// import App from "../App.vue";
 
 if (process.server) {
   Vue.use(ApolloSSR);
@@ -11,17 +10,9 @@ export default {
   async routeResolve({ app, ssr, route, store, error, routeComponents }) {
     if (process.server) {
       try {
-        // await ApolloSSR.prefetchAll(
-        //   app.$apolloProvider,
-        //   [App, ...routeComponents],
-        //   {
-        //     route,
-        //     store
-        //   }
-        // );
-        // ssr.bodyAdd = `<script>window.__APOLLO_STATE__=${JSON.stringify(
-        //   ApolloSSR.getStates(app.$apolloProvider)
-        // )}</script>`;
+        ssr.bodyAdd = `<script>window.__APOLLO_STATE__=${JSON.stringify(
+          ApolloSSR.getStates(app.$apolloProvider)
+        )}</script>`;
       } catch (err) {
         error(err);
       }
